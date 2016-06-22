579c72eda2a37538919a2bf43c9df01e34e03433
Cesar Toledano <cesar.toledano.luna@MTYTM15065X0K.dir.svc.accenture.com>
2016-06-22 12:15:01 -0500

    moving git and gradle to common technology repository

--- gradle/pluralsight-exercises/1-gradle-fundamentals-m1-exercise-files/Gradle/FirstJava/src/main/java/com/pluralsight/Hello.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight;
-
-public class Hello {
-    public static void main(String[] args){
-	System.out.println("Hello World");
-    }
-}
--- gradle/pluralsight-exercises/7-gradle-fundamentals-m7-exercise-files/jacket/migrations/common/1__Create_users_table.sql
+++
@@ -1,6 +0,0 @@
-create table USERS (
-    ID int,
-    EMAIL varchar(100) not null,
-    NAME varchar(100) not null,
-    PASSWORD varchar(100) not null
-);
