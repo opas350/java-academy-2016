34d0261004ff1b3ac423f86c523003f89ecdd9d7
opas350 <opas350@gmail.com>
2016-11-09 06:54:34 -0600

    Add Exercise1.java

---
+++ java-1/exercises/exercises/java/Exercise1.java
@@ -0,0 +1,14 @@
+package exercises.java;
+
+/* First Exercise
+ * javac exercises/java/Exercise1.java
+ * java exercises.java.Exercise1
+ */
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+}
