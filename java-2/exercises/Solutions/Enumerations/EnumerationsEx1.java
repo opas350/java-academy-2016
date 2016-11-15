5b7a003916a8659e8a77275c1aa9ece4faa8e7d1
opas350 <opas350@gmail.com>
2016-11-15 16:11:58 -0600

    Update the comments because the file includes exercise 1 and 3

--- java-2/exercises/Solutions/Enumerations/EnumerationsEx1.java
+++ java-2/exercises/Solutions/Enumerations/EnumerationsEx1.java
@@ -1,11 +1,14 @@
 /* 
-	Exercise 1 of Enumerations
+	Exercise 1 and 3 of Enumerations
 	Author: Javier Reyes
 */
-// Write a simple code for a simple enum example
+// 1.- Write a simple code for a simple enum example
+// 3.-Write an example to iterate all values of an enum.
+
 
 public class EnumerationsEx1 {
 
+	// Code for enum example
 	public enum Planets {
 
 		Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
@@ -13,6 +16,7 @@ public class EnumerationsEx1 {
 
 	public static void main(String[] args) {
 
+		// code for iterate values of enum
 		for(Planets num : Planets.values()){
 			System.out.println(num);
 		}
